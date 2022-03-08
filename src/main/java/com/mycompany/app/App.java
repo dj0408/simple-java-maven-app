32e8bbf9d0f4a4e3e5d486e75d8cc487560f5631
dj0408 <94752391+dj0408@users.noreply.github.com>
2022-03-08 21:42:30 +0530

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -3,7 +3,7 @@ package com.mycompany.app;
 /**
  * Hello world!
  */
-  class Car
+/**  class Car
 {  String name,model;
    int price;
  public Car (String name , String model , int price)
@@ -30,7 +30,7 @@ package com.mycompany.app;
        System.out.println("This car starts like a race car");}
   
   
-  } 
+  }  */
 public class App
 {
 
