41bb441adfa84a8776cf8b504ea72f4142380666
dj0408 <94752391+dj0408@users.noreply.github.com>
2022-03-08 11:26:59 +0530

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -17,7 +17,7 @@ public class App
     private final String getMessage() {
         return message;
     } 
-    public static String carprice(String str)
+    public  String carprice(String str)
     {  System.out.println("we are inside carprice");
         return str + " is expensive";
          }
